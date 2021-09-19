37e737c1bce37c6a09254015ea3688e47ed14ac9
Nancy J.Melucci <drnanjo@aol.com>
2021-09-19 07:01:19 -0700

    Added footer by data object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import './footer.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
 import Products from './pages/Products';
+import Footer from "./Footer";
+
+import * as serviceWorker from "./serviceWorker";
 
 function App() {
   return ( 
@@ -17,9 +21,10 @@ function App() {
           <Route path='/reports' component={Reports} />
           <Route path='/products' component={Products} />
         </Switch>
+		<Footer/>
       </Router>      
     </>
   );
 }
-
+serviceWorker.unregister();
 export default App;
